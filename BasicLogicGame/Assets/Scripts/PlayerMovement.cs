f5997d426d7ea8ed7d57eb2c3d4e4c0d3fd2a5e3
Arkadiusz Woźniak <kkerakk@gmail.com>
2020-10-15 12:34:57 +0200

    PlayerMovement script completed

--- BasicLogicGame/Assets/Scripts/PlayerMovement.cs
+++ BasicLogicGame/Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,37 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     private float speed = 12f;
-    private float gravity = -9.81f;
+    private float gravity = -29.81f; // default: 9.81f
+    private float jumpHeight = 3f;
+
+    [SerializeField] private Transform groundCheck;
+    private float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1.5f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -1.5f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
